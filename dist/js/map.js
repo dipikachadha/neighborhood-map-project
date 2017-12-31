210698f5525be54a7191b7e3c05e8cbcf07455bb
Dipika Chadha <dipika.chadha@gmail.com>
2017-12-31 13:03:56 -0500

    Add minified distribution code

---
+++ dist/js/map.js
@@ -0,0 +1,1 @@
+function initMap(){const e=new google.maps.Map(document.getElementById("map"),{center:{lat:35.7881812,lng:-78.7951261},zoom:12,style:myMapStyles});myPOIs.map(o=>{new google.maps.Marker({position:o.location,map:e})})}
