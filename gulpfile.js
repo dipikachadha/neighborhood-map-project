3831061067681db7727c857d5932b5adf336633e
Dipika Chadha <dipika.chadha@gmail.com>
2017-12-31 13:01:35 -0500

    Clean minification tasks, add watch and default

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,43 @@ const gulp = require('gulp');
 const uglifyes = require('uglify-es');
 const composer = require('gulp-uglify/composer');
 const uglify = composer(uglifyes, console);
-
-gulp.task('compress-js', () => {
-    return gulp.src('src/js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js/'))
-});
-
 const cleanCSS = require('gulp-clean-css');
-
-gulp.task('compress-css', () => {
-  return gulp.src('src/css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist/css/'));
-});
-
 const htmlmin = require('gulp-htmlmin');
 
-gulp.task('compress-html', function() {
-  return gulp.src('src/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'));
+const paths = {
+  origJS: "src/js/*.js",
+  origCSS: "src/css/*.css",
+  origHTML: "src/*.html",
+  destJS: "dist/js/",
+  destCSS: "dist/css/",
+  destHTML: "dist/"
+}
+
+gulp.task('compress-js', () => {
+    return gulp.src(paths.origJS)
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.destJS))
 });
+
+gulp.task('compress-css', () => {
+  return gulp.src(paths.origCSS)
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest(paths.destCSS));
+});
+
+gulp.task('compress-html', function() {
+  return gulp.src(paths.origHTML)
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(paths.destHTML));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.origJS, ['compress-js']);
+  gulp.watch(paths.origCSS, ['compress-css']);
+  gulp.watch(paths.origHTML, ['compress-html']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default',
+  ['watch', 'compress-js', 'compress-css', 'compress-html']);
