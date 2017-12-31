7f57c1cd7f6599aee03d7a943ce8fc5d47f2c3fb
Dipika Chadha <dipika.chadha@gmail.com>
2017-12-31 11:39:52 -0500

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
 //script paths
-var jsFiles = 'js/*.js',
+var jsFiles = 'src/js/*.js',
     jsDest = 'dist/js';
 
 gulp.task('scripts', function() {
