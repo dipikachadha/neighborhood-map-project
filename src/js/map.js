b4c3a80e58659eb3110d6216e8b5b23abc014c50
Dipika Chadha <dipika.chadha@gmail.com>
2017-12-31 17:46:52 -0500

    Add comments to explain map API functions

--- src/js/map.js
+++ src/js/map.js
@@ -1,3 +1,5 @@
+// The file contains all utility functions related to map and map
+// objects such as markers.
 function initMap() {
     // The area where the map would center on
     const cary_NC = {lat: 35.7881812, lng: -78.7951261};
@@ -10,6 +12,7 @@ function initMap() {
     });
 }
 
+// Function to create custom styled markers for the map.
 function makeMarkerIcon(markerColor) {
   var markerImage = new google.maps.MarkerImage(
     'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
@@ -41,19 +44,28 @@ function setMarkerAnimations (marker) {
   });
 }
 
+// Function to update markers on the map as per search results.
 function updateMarkers (fullList, filteredList, map) {
-// Add and remove markers as per the list
-// Remove markers for elements not present in filter
-_.difference(fullList, filteredList).forEach(
-  POI => POI.mapMarker.setMap(null)
-);
+  // Add and remove markers as per the filtered list. This is done in
+  // a two step approach:
+  // 1. All fullList markers that are absent in filteredList are removed.
+  // 2. All markers common to fullList and filteredList are added.
+  // The second step seems redundant, but is essential to keep away from
+  // losing all the markers over subsequent searches.
 
-// Add ones in the intersection to avoid losing all markers
-_.intersection(fullList, filteredList).forEach(
-  POI => POI.mapMarker.setMap(map)
-);
+  // Remove markers for elements not present in filter
+  _.difference(fullList, filteredList).forEach(
+    POI => POI.mapMarker.setMap(null)
+  );
+
+  // Add ones in the intersection to avoid losing all markers
+  _.intersection(fullList, filteredList).forEach(
+    POI => POI.mapMarker.setMap(map)
+  );
 }
 
+// Function to create bounce animations on the markers. Assumes
+// correct `this' context.
 function toggleBounce() {
   if (this.getAnimation() !== null) {
       this.setAnimation(null);
