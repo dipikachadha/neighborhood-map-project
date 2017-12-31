f51230cb6a231d38cf9c2e3805193a415131f99c
Dipika Chadha <dipika.chadha@gmail.com>
2017-12-31 09:41:13 -0500

    Successful display of map

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
+var map;
 function initMap() {
+  // The area where the map would center on
+  const cary_NC = {lat: 35.7881812, lng: -78.7951261};
   // Constructor creates a new map - only center and zoom are required.
-  map = new google.maps.Map(document.getElementById('map-area'), {
-    center: {lat: 35.7881812, lng: -78.7951261},
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: cary_NC,
     zoom: 12,
-  });
+    });
 
-  const largeInfowindow = new google.maps.InfoWindow();
+  const marker = new google.maps.Marker({
+    position: cary_NC,
+    map: map
+    });
 }
