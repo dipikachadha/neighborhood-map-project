8d9b5c55f5ca9fc1287333d494ad0355e86c1823
Dipika Chadha <dipika.chadha@gmail.com>
2018-01-01 16:46:50 -0500

    Efforts to add list hiding function

--- src/js/css_helpers.js
+++ src/js/css_helpers.js
@@ -45,3 +45,9 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
+// Utility function to withdraw the list menu on clicking menu-toggle
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("active");
+});
