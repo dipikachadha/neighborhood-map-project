50a879ce9c0f5e104e4d2d22ffe471d7df738604
dipikachadha <dipika.chadha@gmail.com>
2018-01-02 10:36:29 -0500

    Remove toggle function -- can be handled via KO

--- src/js/css_helpers.js
+++ src/js/css_helpers.js
@@ -46,8 +46,3 @@ function dragElement(elmnt) {
   }
 }
 
-// Utility function to withdraw the list menu on clicking menu-toggle
-$("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#wrapper").toggleClass("active");
-});
